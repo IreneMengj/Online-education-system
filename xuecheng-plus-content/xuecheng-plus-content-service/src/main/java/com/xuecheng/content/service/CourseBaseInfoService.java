07e86e035a012d22ede01a8d91f4b6c41262a84c
irene <mengjiayu2021@gmail.com>
2023-01-25 16:36:36 +0000

    commit query content API

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,32 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+
+import com.xuecheng.model.dto.QueryCourseParamsDto;
+import com.xuecheng.model.dto.QueryCourseParamsDto;
+import com.xuecheng.model.po.CourseBase;
+import com.xuecheng.model.po.CourseBase;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @description 课程管理service
+ * @author Mr.M
+ * @date 2022/10/8 9:44
+ * @version 1.0
+ */
+public interface CourseBaseInfoService {
+
+
+ /**
+  * @description 课程查询
+  * @param params 分页参数
+  * @param queryCourseParamsDto 查询条件
+  * @return com.xuecheng.base.model.PageResult<com.xuecheng.content.model.po.CourseBase>
+  * @author Mr.M
+  * @date 2022/10/8 9:46
+ */
+  public PageResult<CourseBase>  queryCourseBaseList(PageParams params, QueryCourseParamsDto queryCourseParamsDto);
+
+
+}
