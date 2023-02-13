edde237af2424b2a7057630a78ba03afac0211af
irene <mengjiayu2021@gmail.com>
2023-02-13 12:59:39 +0000

    commit order service

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -55,37 +55,18 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
 
     @Override
     public PageResult<CourseBase> queryCourseBaseList(PageParams params, QueryCourseParamsDto queryCourseParamsDto) {
+        LambdaQueryWrapper<CourseBase> wrapper=new LambdaQueryWrapper<>();
+        wrapper.like(StringUtils.isNotEmpty(queryCourseParamsDto.getCourseName()),CourseBase::getName,queryCourseParamsDto.getCourseName());
+        wrapper.eq(StringUtils.isNotEmpty(queryCourseParamsDto.getPublishStatus()),CourseBase::getStatus,queryCourseParamsDto.getPublishStatus());
+        wrapper.eq(StringUtils.isNotEmpty(queryCourseParamsDto.getAuditStatus()),CourseBase::getAuditStatus,queryCourseParamsDto.getAuditStatus());
 
-        LambdaQueryWrapper<CourseBase> queryWrapper = new LambdaQueryWrapper<>();
-
-        //拼接查询条件
-        //根据课程名称模糊查询  name like '%名称%'
-        queryWrapper.like(StringUtils.isNotEmpty(queryCourseParamsDto.getCourseName()),CourseBase::getName,queryCourseParamsDto.getCourseName());
-
-        //根据课程审核状态
-        queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParamsDto.getAuditStatus()),CourseBase::getAuditStatus,queryCourseParamsDto.getAuditStatus());
-
-        //根据课程发布状态
-        queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParamsDto.getPublishStatus()),CourseBase::getStatus,queryCourseParamsDto.getPublishStatus());
-
-
-        //分页参数
         Page<CourseBase> page = new Page<>(params.getPageNo(), params.getPageSize());
 
-
-        //分页查询E page 分页参数, @Param("ew") Wrapper<T> queryWrapper 查询条件
-        Page<CourseBase> pageResult = courseBaseMapper.selectPage(page, queryWrapper);
-
-        //数据
-        List<CourseBase> items = pageResult.getRecords();
-        //总记录数
+        Page<CourseBase> pageResult= courseBaseMapper.selectPage(page,wrapper);
+        List<CourseBase> records = pageResult.getRecords();
         long total = pageResult.getTotal();
-
-
-        //准备返回数据 List<T> items, long counts, long page, long pageSize
-        PageResult<CourseBase> courseBasePageResult = new PageResult<>(items, total, params.getPageNo(), params.getPageSize());
-
-        return courseBasePageResult;
+        PageResult<CourseBase> pageResult1= new PageResult<>(records,total, params.getPageNo(), params.getPageSize());
+        return pageResult1;
     }
 
     @Transactional
